fa8d2bf66685e06f22b9b4fb754ea06e543085c7
Hanna Pshanichnaya <hannapshanich@gmail.com>
2022-07-26 00:12:16 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,22 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import GoogleLogin from 'react-google-login';
+import { useState } from 'react';
 
 function App() {
+  const [loginData, setLogInData] = useState(
+    localStorage.getItem('loginData')
+      ? JSON.parse(localStorage.getItem('loginData'))
+      : null
+  );
+
+  const handleFailure = (result) => {
+    alert('failure: ' + result);
+  };
+
+  const handleLogin = async (googleData) => {
+    const res = await fetch('/api/login-google', {
+      method: 'POST',
+      body: JSON.stringify({
+        token: googleData.tokenId,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await res.json();
+    setLogInData(data);
+    localStorage.setItem('loginData', JSON.stringify(data));
+  };
+
+  const handleLogOut = () => {
+    localStorage.removeItem(loginData);
+    setLogInData(null);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React login with Google</h1>
+        <div>
+          {loginData ? (
+            <div>
+              <h2>You logged in as {loginData.email}</h2>
+              <button onClick={handleLogOut}>LogOut</button>
+            </div>
+          ) : (
+            <GoogleLogin
+              clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+              buttonText="Log in with Google"
+              onSuccess={handleLogin}
+              onFailure={handleFailure}
+              cookiePolicy={'single_host_origin'}
+            ></GoogleLogin>
+          )}
+        </div>
       </header>
     </div>
   );
